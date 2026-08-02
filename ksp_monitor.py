1b311c3c2237c2a36c8345ca287b599862b82bfb
srwnyzyrtmwpt-maker <srwnyzyrtmwpt@gmail.com>
2026-08-03 01:09:13 +0300

    Update ksp_monitor.py

--- ksp_monitor.py
+++ ksp_monitor.py
@@ -1,1 +1,12 @@
-print("KSP Pokemon Alert Bot started")
+import requests
+
+url = "https://ksp.co.il/web/cat/1215?search=%D7%A7%D7%9C%D7%A4%D7%99%20%D7%A4%D7%95%D7%A7%D7%99%D7%9E%D7%95%D7%9F"
+
+response = requests.get(url)
+
+print("Status:", response.status_code)
+
+if "Pokemon" in response.text or "פוקימון" in response.text:
+    print("מצאתי מוצרי פוקימון!")
+else:
+    print("לא נמצא פוקימון")
